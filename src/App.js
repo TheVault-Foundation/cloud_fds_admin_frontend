88debf1e60e0cb4c10f6dd9d248900d1533e8dcf
Son Pham <pxson.2903@gmail.com>
2019-09-16 00:05:19 +0700

    Config App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { compose } from "recompose";
+import { withNamespaces } from "react-i18next";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import asyncComponent from "./hoc/asyncComponent";
+import { path } from "ramda";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+// const AsyncServerRepair = asyncComponent(() => {
+//   return import("./containers/ServerRepair/Loadable");
+// });
+
+class App extends Component {
+  render() {
+    let routes = (
+      <Switch>
+        <Route exact path="/" component={null} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    return (
+      <div>
+        {routes}
+        {/* <LoadingSpinner /> */}
+        {/* <ErrorPopup /> */}
+        {/* <AsyncServerRepair /> */}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    // loggedIn: state.auth.loggedIn,
+    // isRepair: state.serverStatus.isRepair
+    loggedIn: false,
+    isRepair: false
+  };
+};
+
+export default compose(
+  withNamespaces("common", { wait: true }),
+  withRouter,
+  connect(
+    mapStateToProps,
+    {}
+  )
+)(App);
