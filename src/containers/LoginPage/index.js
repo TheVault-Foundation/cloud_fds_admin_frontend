986d960a9281d4cf52cd061bbd74d7378d382583
Son Pham <pxson.2903@gmail.com>
2019-09-16 08:40:59 +0700

    Add Login Page

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,38 +1,151 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { withNamespaces } from 'react-i18next';
-import i18n from 'i18next';
-import { compose } from 'recompose';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { withNamespaces } from "react-i18next";
+import i18n from "i18next";
+import { compose } from "recompose";
 
+// nodejs library that concatenates classes
+import classnames from "classnames";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+import AuthHeader from "components/Headers/AuthHeader.jsx";
 class LoginPage extends Component {
-  state = {
-    
-  };
+  state = {};
 
   componentDidMount() {
-    this.props.history.push('/login');
+    this.props.history.push("/login");
   }
 
   render() {
     return (
-      null
+      <>
+        <AuthHeader title="TheVault Foundation" lead="Admin dasboard" />
+        <Container className="mt--8 pb-5">
+          <Row className="justify-content-center">
+            <Col lg="5" md="7">
+              <Card className="bg-secondary border-0 mb-0">
+                <CardBody className="px-lg-5 py-lg-5">
+                  <div className="text-center text-muted mb-4">
+                    <small>Login</small>
+                  </div>
+                  <Form role="form">
+                    <FormGroup
+                      className={classnames("mb-3", {
+                        focused: this.state.focusedEmail
+                      })}
+                    >
+                      <InputGroup className="input-group-merge input-group-alternative">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="ni ni-email-83" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          placeholder="Email"
+                          type="email"
+                          onFocus={() => this.setState({ focusedEmail: true })}
+                          onBlur={() => this.setState({ focusedEmail: false })}
+                        />
+                      </InputGroup>
+                    </FormGroup>
+                    <FormGroup
+                      className={classnames({
+                        focused: this.state.focusedPassword
+                      })}
+                    >
+                      <InputGroup className="input-group-merge input-group-alternative">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="ni ni-lock-circle-open" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          placeholder="Password"
+                          type="password"
+                          onFocus={() =>
+                            this.setState({ focusedPassword: true })
+                          }
+                          onBlur={() =>
+                            this.setState({ focusedPassword: false })
+                          }
+                        />
+                      </InputGroup>
+                    </FormGroup>
+                    <div className="custom-control custom-control-alternative custom-checkbox">
+                      <input
+                        className="custom-control-input"
+                        id=" customCheckLogin"
+                        type="checkbox"
+                      />
+                      <label
+                        className="custom-control-label"
+                        htmlFor=" customCheckLogin"
+                      >
+                        <span className="text-muted">Remember me</span>
+                      </label>
+                    </div>
+                    <div className="text-center">
+                      <Button className="my-4" color="info" type="button">
+                        Sign in
+                      </Button>
+                    </div>
+                  </Form>
+                </CardBody>
+              </Card>
+              <Row className="mt-3">
+                <Col xs="6">
+                  <a
+                    className="text-light"
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                  >
+                    <small>Forgot password?</small>
+                  </a>
+                </Col>
+                <Col className="text-right" xs="6">
+                  <a
+                    className="text-light"
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                  >
+                    <small>Create new account</small>
+                  </a>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    loggedIn: false,
+    loggedIn: false
   };
 };
 
 export default compose(
-  withNamespaces('login', { wait: true }),
+  withNamespaces("login", { wait: true }),
   withRouter,
   connect(
     mapStateToProps,
-    {
-    }
+    {}
   )
 )(LoginPage);
