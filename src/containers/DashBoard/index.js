8b016b5cbc8ef7544e57177a366defe7db2fc296
Son Pham <son@thevault.foundation>
2019-09-21 17:04:35 +0700

    Check authentication for every admin compoents

--- src/containers/DashBoard/index.js
+++ src/containers/DashBoard/index.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import DashBoard from "../../components/DashBoard";
+import { path } from "ramda";
 
 class DashBoardContainer extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+      if (!this.props.isAuthenticated) {
+          this.props.history.push("/");
+      }
+  }
 
   render() {
     return <DashBoard {...this.props} />;
@@ -12,7 +17,9 @@ class DashBoardContainer extends Component {
 }
 
 const mapStateToProps = state => {
-  return {};
+  return {
+      isAuthenticated: path(["auth", "data", "access_token"], state)
+  };
 };
 
 export default withRouter(
